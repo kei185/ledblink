7bf8a2182e2c32a6a5e8e7dee39a27dda18458d4
kei185 <wajacack@gmail.com>
2026-08-18 06:48:03 +0900

    add timer setting

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,7 +18,6 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-// #include "loop.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -41,6 +40,8 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim2;
+
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
@@ -51,6 +52,7 @@ UART_HandleTypeDef huart2;
 void        SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_TIM2_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -73,8 +75,7 @@ int main(void)
 
         /* MCU Configuration--------------------------------------------------------*/
 
-        /* Reset of all peripherals, Initializes the Flash interface and the Systick.
-         */
+        /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
         HAL_Init();
 
         /* USER CODE BEGIN Init */
@@ -91,6 +92,7 @@ int main(void)
         /* Initialize all configured peripherals */
         MX_GPIO_Init();
         MX_USART2_UART_Init();
+        MX_TIM2_Init();
         /* USER CODE BEGIN 2 */
 
         /* USER CODE END 2 */
@@ -99,9 +101,6 @@ int main(void)
         /* USER CODE BEGIN WHILE */
         while (1) {
                 /* USER CODE END WHILE */
-                // loop();
-                HAL_Delay(1000);
-                HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
 
                 /* USER CODE BEGIN 3 */
         }
@@ -151,6 +150,58 @@ void SystemClock_Config(void)
         if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
                 Error_Handler();
         }
+}
+
+/**
+ * @brief TIM2 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_TIM2_Init(void)
+{
+
+        /* USER CODE BEGIN TIM2_Init 0 */
+
+        /* USER CODE END TIM2_Init 0 */
+
+        TIM_ClockConfigTypeDef  sClockSourceConfig = {0};
+        TIM_MasterConfigTypeDef sMasterConfig      = {0};
+        TIM_IC_InitTypeDef      sConfigIC          = {0};
+
+        /* USER CODE BEGIN TIM2_Init 1 */
+
+        /* USER CODE END TIM2_Init 1 */
+        htim2.Instance               = TIM2;
+        htim2.Init.Prescaler         = 0; // PSC
+        htim2.Init.CounterMode       = TIM_COUNTERMODE_UP;
+        htim2.Init.Period            = 4294967295; // ARR
+        htim2.Init.ClockDivision     = TIM_CLOCKDIVISION_DIV1;
+        htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+        if (HAL_TIM_Base_Init(&htim2) != HAL_OK) {
+                Error_Handler();
+        }
+        sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+        if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK) {
+                Error_Handler();
+        }
+        if (HAL_TIM_IC_Init(&htim2) != HAL_OK) {
+                Error_Handler();
+        }
+        sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+        sMasterConfig.MasterSlaveMode     = TIM_MASTERSLAVEMODE_DISABLE;
+        if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK) {
+                Error_Handler();
+        }
+        sConfigIC.ICPolarity  = TIM_INPUTCHANNELPOLARITY_RISING;
+        sConfigIC.ICSelection = TIM_ICSELECTION_DIRECTTI;
+        sConfigIC.ICPrescaler = TIM_ICPSC_DIV1;
+        sConfigIC.ICFilter    = 0;
+        if (HAL_TIM_IC_ConfigChannel(&htim2, &sConfigIC, TIM_CHANNEL_1) != HAL_OK) {
+                Error_Handler();
+        }
+        /* USER CODE BEGIN TIM2_Init 2 */
+
+        /* USER CODE END TIM2_Init 2 */
 }
 
 /**
